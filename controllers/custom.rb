a10bfdc77aa04bfe4935836dc27a0b892e30da03
Ezra Chang <ezra.mintao.chang@gmail.com>
2021-07-25 09:29:39 -0500

    feat: adds blog (#26)

--- controllers/custom.rb
+++ controllers/custom.rb
@@ -39,6 +39,10 @@ class CustomController < Sinatra::Base
   end
 
   not_found do
-    slim :notfound, locals: { **ContentHelpers.nf_404, url: request.url }
+    slim :notfound, locals: {
+      **ContentHelpers.nf_404,
+      description: 'Lost the plot, have you?',
+      url: request.url
+    }
   end
 end
